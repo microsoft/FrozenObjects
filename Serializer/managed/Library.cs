dd2d09dca2d971f024c6e5e30929c40a62dbc879
Mukul Sabharwal <mjsabby@gmail.com>
2019-07-08 23:28:31 -0700

    Add some code comments to convey key concepts (#12)

--- Serializer/managed/Library.cs
+++ Serializer/managed/Library.cs
@@ -18,7 +18,7 @@
         {
             Dictionary<Type, int> typeTokenMap = SerializeDataAndBuildTypeTokenMap(o, outputDataPath);
 
-            (IEnumerable<Assembly> allAssemblies, IEnumerable<Type> allTypes) = ComputeAssemblyAndTypeClosure(typeTokenMap);
+            (HashSet<Assembly> allAssemblies, HashSet<Type> allTypes) = ComputeAssemblyAndTypeClosure(typeTokenMap);
 
             MetadataBuilder metadataBuilder = CreateMetadataBuilder(Path.GetFileName(outputAssemblyFilePath), Path.GetFileNameWithoutExtension(outputAssemblyFilePath), version);
             
@@ -29,6 +29,25 @@
             SerializeCompanionAssembly(metadataBuilder, outputAssemblyFilePath, outputNamespace, typeName, methodName, typeTokenMap, typeToTypeSpecMap, privateKeyOpt);
         }
 
+        /// <summary>
+        /// Responsible for serializing the object graph (<param name="o"></param> to <param name="outputDataPath"></param>
+        /// </summary>
+        /// <remarks>
+        /// Loads the native shared library that implements walking the object graph using GCDesc. The reason the functionality
+        /// is in a native library is because we do a managed calli into this library as a mechanism to supress GC while we serialize
+        /// the object graph to disk.
+        ///
+        /// Each serialized object has a placeholder token written in place of the MethodTable*.
+        ///
+        /// A tuple of each unique type we encounter and its placeholder token are returned from the native library. We in turn convert
+        /// this data into a Dictionary`[System.Type, System.Int32], so that the remainder of the work can be done using System.Reflection
+        ///
+        /// So to summarize, we write the object graph to disk and get all unique types in the form of a Dictionary`[System.Type, System.Int32]
+        ///
+        /// Next, please read <seealso cref="ComputeAssemblyAndTypeClosure"/>.
+        /// </remarks>
+        /// <param name="o">object to serialize</param>
+        /// <param name="outputDataPath">file path for the serialized object graph</param>
         private static unsafe Dictionary<Type, int> SerializeDataAndBuildTypeTokenMap(object o, string outputDataPath)
         {
             IntPtr handle;
@@ -86,7 +105,24 @@
             }
         }
 
-        private static Tuple<IEnumerable<Assembly>, IEnumerable<Type>> ComputeAssemblyAndTypeClosure(Dictionary<Type, int> typeTokenMap)
+        /// <summary>
+        /// Responsible for finding the closure of all Assembly and Types reachable from the original
+        /// set of Types that were part of the input object graph.
+        /// </summary>
+        /// <remarks>
+        ///
+        /// We first put all the types we saw in the object walk into a queue, then find all the base types, and
+        /// if the types were nested, the heirarchy of those types. If the type was generic then their arguments, which
+        /// in turn go find all theirs, so on and so forth.
+        ///
+        /// Ultimately the goal of this function is to get all the types and assemblies that could be needed when encoding the signatures.
+        ///
+        /// It is quite possible that some of these types may not require to be encoded, and that's ok.
+        ///
+        /// Next, please read <seealso cref="AccumulateAssemblyReferenceHandles"/>.
+        /// </remarks>
+        /// <param name="typeTokenMap">The unique set of types that were in the input object graph</param>
+        private static Tuple<HashSet<Assembly>, HashSet<Type>> ComputeAssemblyAndTypeClosure(Dictionary<Type, int> typeTokenMap)
         {
             var typeQueue = new Queue<Type>();
 
@@ -148,10 +184,20 @@
                 typeQueue.Dequeue();
             }
 
-            return new Tuple<IEnumerable<Assembly>, IEnumerable<Type>>(allAssemblies, allTypes);
+            return new Tuple<HashSet<Assembly>, HashSet<Type>>(allAssemblies, allTypes);
         }
 
-        private static Dictionary<Assembly, AssemblyReferenceHandle> AccumulateAssemblyReferenceHandles(MetadataBuilder metadataBuilder, IEnumerable<Assembly> allAssemblies)
+        /// <summary>
+        /// Responsible for taking all the unique Assembly objects and just blindly writing an AssemblyRef into the metadata
+        /// </summary>
+        /// <remarks>
+        ///
+        /// Nothing particularly interesting here, please read <seealso cref="AccumulateTypeReferenceHandles"/>
+        ///
+        /// </remarks>
+        /// <param name="metadataBuilder">the metadata builder</param>
+        /// <param name="allAssemblies">unique set of assemblies encountered in our closure walk</param>
+        private static Dictionary<Assembly, AssemblyReferenceHandle> AccumulateAssemblyReferenceHandles(MetadataBuilder metadataBuilder, HashSet<Assembly> allAssemblies)
         {
             var assemblyReferenceHandleMap = new Dictionary<Assembly, AssemblyReferenceHandle>();
 
@@ -179,7 +225,33 @@
             return assemblyReferenceHandleMap;
         }
 
-        private static Dictionary<Type, TypeReferenceHandle> AccumulateTypeReferenceHandles(MetadataBuilder metadataBuilder, Dictionary<Assembly, AssemblyReferenceHandle> assemblyReferenceHandleMap, IEnumerable<Type> allTypes)
+        /// <summary>
+        /// Responsible for taking all the unique Type objects and putting the TypeRefs into the metadata
+        /// </summary>
+        /// <remarks>
+        ///
+        /// A couple of interesting things to point out:
+        ///
+        /// (1) Notice that nested types are handled by putting them into this <code>typeList</code>, and then the list in walked in reverse order
+        ///     so that all the TypeRefs can be found by subsequent dictionary lookups.
+        ///
+        /// (2) The resolutionScope is taken care of by checking if there is a declaring type or not, and by (1) we'll ensure that we always do it
+        ///     an order that those dictionary lookups can be satisfied. And if you're a nested type your namespace must be empty. System.Reflection
+        ///     doesn't return an empty namespace, so we had to do some work there.
+        ///
+        /// (3) Note that we need to get ensure we're working with the generic type definition, otherwise we'll serialize the same typerefs multiple times
+        ///     and fail PE Verification.
+        ///
+        /// After this function completes we can be certain that all typerefs & assemblyrefs needed for the next phase, encoding of the typespec signature
+        /// are a dictionary lookup away.
+        ///
+        /// Please read <seealso cref="AccumulateTypeSpecificationHandles"/>
+        ///
+        /// </remarks>
+        /// <param name="metadataBuilder"></param>
+        /// <param name="assemblyReferenceHandleMap"></param>
+        /// <param name="allTypes"></param>
+        private static Dictionary<Type, TypeReferenceHandle> AccumulateTypeReferenceHandles(MetadataBuilder metadataBuilder, Dictionary<Assembly, AssemblyReferenceHandle> assemblyReferenceHandleMap, HashSet<Type> allTypes)
         {
             var uniqueTypeRefMap = new Dictionary<Type, TypeReferenceHandle>();
 
@@ -222,6 +294,36 @@
             return uniqueTypeRefMap;
         }
 
+        /// <summary>
+        /// Responsible for encoding the typespec signatures for each of the unique types we encountered in the input object graph.
+        /// </summary>
+        /// <remarks>
+        ///
+        /// At this point we have all the pieces needed to encoder a TypeSpec signature for each unique type we saw in the object graph.
+        ///
+        /// We have to use the short-form signatures for some of those primitive type codes, but besides that we just handle the ECMA-335 II.23.2.12 Type
+        /// production rules except for the stuff we know we don't want or can't encounter on a heap.
+        ///
+        /// This includes:
+        ///
+        /// FNPTR MethodDefOrRefSig
+        /// MVAR number
+        /// VAR number
+        ///
+        /// In addition to that I don't think we want these:
+        ///
+        /// PTR Type
+        /// PTR VOID
+        ///
+        /// You can see all the stuff we handle here, <seealso cref="HandleType"/>
+        /// and next on the reading list is <seealso cref="SerializeCompanionAssembly"/>
+        /// 
+        /// </remarks>
+        /// 
+        /// <param name="metadataBuilder">the metadata builder</param>
+        /// <param name="uniqueTypeRefMap">dictionary of all possible types</param>
+        /// <param name="typeTokenMap">the actual unique set of types we encountered</param>
+        /// <returns></returns>
         private static Dictionary<Type, TypeSpecificationHandle> AccumulateTypeSpecificationHandles(MetadataBuilder metadataBuilder, Dictionary<Type, TypeReferenceHandle> uniqueTypeRefMap, Dictionary<Type, int> typeTokenMap)
         {
             var primitiveTypeCodeMap = new Dictionary<Type, PrimitiveTypeCode>
@@ -248,7 +350,7 @@
             foreach (var type in typeTokenMap.Keys)
             {
                 var blobBuilder = new BlobBuilder();
-                var encoder = new SignatureTypeEncoder(blobBuilder);
+                var encoder = new BlobEncoder(blobBuilder).TypeSpecificationSignature();
 
                 HandleType(type, ref encoder, primitiveTypeCodeMap, uniqueTypeRefMap);
 
@@ -330,6 +432,30 @@
             }
         }
 
+        /// <summary>
+        /// Responsible for serializing the MethodDefinition that allows the consumer to load the associated blob.
+        /// </summary>
+        /// <remarks>
+        ///
+        /// Generates a method whose signature is `object MethodName(string filePath);`
+        ///
+        /// The file path is the path of the blob associated with this assembly.
+        ///
+        /// The method that this piece of code generates has the glue required for the deserialization to work. Essentially it allocates
+        /// an array of RuntimeTypeHandles, and the index of the array is the the placeholder token we stuffed into the serialized object we wrote to disk.
+        ///
+        /// At deserialization time, the user calls this method which does an `ldtoken TypeSpec` for each of the unique types we saw at serialization time.
+        /// And then the deserializer uses this information to do the fixups as it deserializes using GCDesc.
+        ///
+        /// </remarks>
+        /// <param name="metadataBuilder">the metadata builder</param>
+        /// <param name="outputAssemblyFilePath">the file path of the assembly</param>
+        /// <param name="outputNamespace">the namespace of the assembly</param>
+        /// <param name="typeName">the type of the method</param>
+        /// <param name="methodName">the name of the method</param>
+        /// <param name="typeTokenMap">the type to token place holder map</param>
+        /// <param name="typeToTypeSpecMap">the type to typespec map</param>
+        /// <param name="privateKeyOpt">optional key</param>
         private static void SerializeCompanionAssembly(MetadataBuilder metadataBuilder, string outputAssemblyFilePath, string outputNamespace, string typeName, string methodName, Dictionary<Type, int> typeTokenMap, Dictionary<Type, TypeSpecificationHandle> typeToTypeSpecMap, byte[] privateKeyOpt)
         {
             var netstandardAssemblyRef = metadataBuilder.AddAssemblyReference(metadataBuilder.GetOrAddString("netstandard"), new Version(2, 0, 0, 0), default, metadataBuilder.GetOrAddBlob(new byte[] { 0xCC, 0x7B, 0x13, 0xFF, 0xCD, 0x2D, 0xDD, 0x51 }), default, default);
