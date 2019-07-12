73b579872ec5318763bb39bddb9c33bc14999827
Mukul Sabharwal <muks@microsoft.com>
2019-07-11 23:37:11 -0700

    Native docker build files only

--- build-native.sh
+++ build-native.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-clang++ -shared -o Serializer/native/Microsoft.FrozenObjects.Serializer.Native.so --no-undefined -Wno-invalid-noreturn -fPIC -std=c++11 -O2 Serializer/native/Microsoft.FrozenObjects.Serializer.Native.cpp
+clang++ -shared -o $1/Microsoft.FrozenObjects.Serializer.Native.so --no-undefined -Wno-invalid-noreturn -fPIC -std=c++11 -O2 Serializer/native/Microsoft.FrozenObjects.Serializer.Native.cpp
