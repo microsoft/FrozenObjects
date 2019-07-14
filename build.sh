3d1ff8e53bfc5cf1331d8ad2292c63c73fc6bda7
Mukul Sabharwal <mjsabby@gmail.com>
2019-07-14 05:44:17 -0700

    Fix sln (#27)

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-./build-managed.sh
-./build-native.sh
+dotnet build BuildTools/Microsoft.FrozenObjects.BuildTools.csproj -c Debug
+dotnet build BuildTools/Microsoft.FrozenObjects.BuildTools.csproj -c Release
+dotnet build Serializer/Microsoft.FrozenObjects.Serializer.csproj -c Debug
+dotnet build Serializer/Microsoft.FrozenObjects.Serializer.csproj -c Release
+dotnet build Deserializer/Microsoft.FrozenObjects.csproj -c Debug
+dotnet build Deserializer/Microsoft.FrozenObjects.csproj -c Release
+
 ./pack.sh
