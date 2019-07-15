756abc5485f99a052c30812a5f4c5645cb49fd7d
Mukul Sabharwal <muks@microsoft.com>
2019-07-14 21:38:29 -0700

    Rename

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-dotnet build BuildTools\Microsoft.FrozenObjects.BuildTools.csproj -c Debug
-dotnet build BuildTools\Microsoft.FrozenObjects.BuildTools.csproj -c Release
+dotnet build tools\Microsoft.FrozenObjects.BuildTools.csproj -c Debug
+dotnet build tools\Microsoft.FrozenObjects.BuildTools.csproj -c Release
 dotnet build src\Microsoft.FrozenObjects.csproj -c Debug
 dotnet build src\Microsoft.FrozenObjects.csproj -c Release
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dotnet build BuildTools/Microsoft.FrozenObjects.BuildTools.csproj -c Debug
-dotnet build BuildTools/Microsoft.FrozenObjects.BuildTools.csproj -c Release
+dotnet build tools/Microsoft.FrozenObjects.BuildTools.csproj -c Debug
+dotnet build tools/Microsoft.FrozenObjects.BuildTools.csproj -c Release
 dotnet build src/Microsoft.FrozenObjects.csproj -c Debug
 dotnet build src/Microsoft.FrozenObjects.csproj -c Release
