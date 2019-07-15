7bac97884a6fcea6e041e0a11fd487102eeb32bf
Mukul Sabharwal <muks@microsoft.com>
2019-07-15 00:05:28 -0700

    Remove MSBuild task and use exec

--- build.cmd
+++
@@ -1,4 +0,0 @@
-dotnet build tools\Microsoft.FrozenObjects.BuildTools.csproj -c Debug
-dotnet build tools\Microsoft.FrozenObjects.BuildTools.csproj -c Release
-dotnet build src\Microsoft.FrozenObjects.csproj -c Debug
-dotnet build src\Microsoft.FrozenObjects.csproj -c Release
--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-dotnet build tools/Microsoft.FrozenObjects.BuildTools.csproj -c Debug
-dotnet build tools/Microsoft.FrozenObjects.BuildTools.csproj -c Release
-dotnet build src/Microsoft.FrozenObjects.csproj -c Debug
-dotnet build src/Microsoft.FrozenObjects.csproj -c Release
